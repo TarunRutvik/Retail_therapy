2fdb3cea8fa785b12843530bdae05d4dd89d1bc7
TarunRutvik <gandeti.tarun@gmail.com>
2024-10-06 19:52:36 +1100

    update source location

---
+++ src/test/java/SimpleWebTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleWebTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tarun\\Downloads\\chromedriver-win64\\chromedriver-win64"); // Update the path
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testHomePage() {
+        driver.get("http://localhost:8080"); // URL to your local test server
+        String title = driver.getTitle();
+        Assert.assertEquals("Expected Title", title); // Change "Expected Title" to the actual title
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
